cc69a3e51eae638485a2b921ea65c4bfd7d309fb
guanlaolin <guanlaolin@163.com>
2018-07-13 16:34:41 +0800

    Add Apache2.0 license

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# crwal-itellyou.cn
+# crwal-itellyou.cn  
+
+## 写在前面
+一个用 Golang 实现的简单的爬取 msdn.itellyou.cn 的小程序。纯属学习目的，无任何恶意，如若无意冒犯了网站作者，烦请您联系我，马上删除。
+
+## 爬取过程
+### 网站结构：
+
+## 联系方式
+邮箱：<a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=HHtpfXJwfXNwdXJcbW0yf3Nx" style="text-decoration:none;"><img src="http://rescdn.qqmail.com/zh_CN/htmledition/images/function/qm_open/ico_mailme_21.png"/></a>
--- core.go
+++ core.go
@@ -1,4 +1,19 @@
-// core.go
+/**
+ *   Copyright 2018 Guanlaolin
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 package main
 
 import (
@@ -10,18 +25,21 @@ import (
 	"strings"
 )
 
+// 类别
 type Cata struct {
-	Name  string // cata name
+	Name  string
 	Id    string
 	Items []Item
 }
 
+// 类别下的具体项，如Win10、Win7...
 type Item struct {
 	Name      string
 	Id        string
 	ItemLangs []ItemLanguage
 }
 
+// 每个具体项有哪些语言类别可供选择
 type ItemLanguage struct {
 	Lang     string
 	Id       string
